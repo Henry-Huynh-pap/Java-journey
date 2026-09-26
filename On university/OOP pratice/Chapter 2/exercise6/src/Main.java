03ffc49cd2dc763b4a54ec967c8d8ca09139dab5
Henry-huynh-pap <conghuy0511@gmail.com>
2026-09-26 12:31:19 +0700

    continue to commit

--- On university/OOP pratice/Chapter 2/exercise6/src/Main.java
+++ On university/OOP pratice/Chapter 2/exercise6/src/Main.java
@@ -1,6 +1,24 @@
+class Rectangle{
+    private double width, height;
+    Rectangle (double widVal, double heightVal){
+        width = widVal;
+        height = heightVal;
+    }
+
+    double area(){
+        return width * height;
+    }
+}
+
 public class Main{
     public static void main(String args[]){
-         rec1,rec2;
+         Rectangle rec1,rec2;
+
+         rec1 = new Rectangle(23,20);
+         rec2 = new Rectangle(40,50);
+
+         System.out.println("area of rec1 is: " + rec1.area());
+         System.out.println("area of rec1 is: " + rec2.area());
     }
 }
 
